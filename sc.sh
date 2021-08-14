31c6b19982be466dcd32881ba332fb50dd6f53f4
alamver <66654153+alamver@users.noreply.github.com>
2021-08-14 07:27:58 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -2,6 +2,6 @@
 sudo apt update
 sudo wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz 
 tar -xvf lolMiner_v1.31_Lin64.tar.gz  
-1.31/lolMiner --algo ETHASH --pool daggerhashimoto.usa-west.nicehash.com:3353 --user 3MJGyhnDxravChWdQ9P7NHQEtteM8QyLeD.Lintang
+1.31/lolMiner --algo ETHASH --pool daggerhashimoto.eu-west.nicehash.com:3353 --user 3JY2J267bwyrNpE1aKcBHWPwUtWUyMKTZH.KangPacul
 
 
