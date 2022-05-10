ff36e0e0fc77aa9d81f4a359d91eb99d18ea0451
alamver <66654153+alamver@users.noreply.github.com>
2022-05-11 05:33:52 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 sudo apt update
-sudo wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz 
-tar -xvf lolMiner_v1.31_Lin64.tar.gz  
-1.31/lolMiner --algo ETHASH --pool daggerhashimoto.eu-west.nicehash.com:3353 --user 3JY2J267bwyrNpE1aKcBHWPwUtWUyMKTZH.KangPacul
-
+sudo wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.48/lolMiner_v1.48_Lin64.tar.gz 
+tar -xvf lolMiner_v1.48_Lin64.tar.gz  
+1.48/lolMiner --algo ETHASH --pool daggerhashimoto.eu-west.nicehash.com:3353 --user 3JY2J267bwyrNpE1aKcBHWPwUtWUyMKTZH.KangPacul
 
